3f8151c064ed2695dd85d16f536046957a6b877f
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-07-30 10:46:19 +0200

    feat(pagination): style for pagination on post

--- components/post/index.js
+++ components/post/index.js
@@ -6,30 +6,32 @@ import style from './post.module.css';
 
 const Post = ({ title, slug, html, hidden, description, date, previous, next }) => {
   return (
-    <Page slug={slug} title={title} description={description}>
-      <Head>
-        {hidden && <meta name="robots" content="noindex" />}
-        {date && <meta name="date" content={date} />}
-      </Head>
-      <div className={style.container}>
-        <span>
-          {slug ? (
-            <b>
-              <span className={style.title}> {title} </span>
-            </b>
-          ) : (
-            ''
-          )}
-        </span>
+    <div>
+      <Page slug={slug} title={title} description={description}>
+        <Head>
+          {hidden && <meta name="robots" content="noindex" />}
+          {date && <meta name="date" content={date} />}
+        </Head>
+        <div className={style.container}>
+          <span>
+            {slug ? (
+              <b>
+                <span className={style.title}> {title} </span>
+              </b>
+            ) : (
+              ''
+            )}
+          </span>
 
-        <article
-          dangerouslySetInnerHTML={{
-            __html: `${html}`,
-          }}
-        />
-        <Navigation previous={previous} next={next} />
-      </div>
-    </Page>
+          <article
+            dangerouslySetInnerHTML={{
+              __html: `${html}`,
+            }}
+          />
+        </div>
+      </Page>
+      <Navigation previous={previous} next={next} />
+    </div>
   );
 };
 
