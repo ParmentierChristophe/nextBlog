0213a3973704fd7b22b8427874b663867dc92e4c
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-07-30 10:46:19 +0200

    feat(pagination): style for pagination on post

--- components/post/navigation.js
+++ components/post/navigation.js
@@ -1,22 +1,30 @@
 import Link from '../link';
+import style from './navigation.module.css';
 
 const Previous = ({ previous, next }) => {
   return (
     <div>
-      <div>
-        {previous && (
-          <Link href="/[slug]" gray as={`/${previous.slug}`}>
-            <div>← {previous.title}</div>
-          </Link>
-        )}
-      </div>
+      <hr />
+      <div className={style.container}>
+        <div className={style.globalPreviousNext}>
+          <div className={style.previous}>
+            <p>Previous</p>
+            {previous && (
+              <Link href="/[slug]" gray as={`/${previous.slug}`}>
+                ← {previous.title}
+              </Link>
+            )}
+          </div>
 
-      <div>
-        {next && (
-          <Link href="/[slug]" gray as={`/${next.slug}`}>
-            <div>{next.title} →</div>
-          </Link>
-        )}
+          <div className={style.next}>
+            <p>Next</p>
+            {next && (
+              <Link href="/[slug]" gray as={`/${next.slug}`}>
+                {next.title} →
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
