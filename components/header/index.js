d487cf22b5ef68058159c79ba1103f4eb348357e
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-07-30 10:08:15 +0200

    add FontAwesome

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import style from './header.module.css';
 import useTheme from '../../lib/theme';
+import useMounted from '../../lib/use-mounted';
 
-const Header = ({ slug, title }) => {
+const Header = () => {
   const { theme, toggleTheme } = useTheme();
+  const isMounted = useMounted();
 
   return (
     <nav className={style.navbar}>
@@ -14,20 +16,20 @@ const Header = ({ slug, title }) => {
               Crispy
             </a>
           </Link>
-          <button onClick={toggleTheme} aria-label="Toggle Theme">
-            AZR
+          <button onClick={toggleTheme} className={style.btn} aria-label="Toggle Theme">
+            {isMounted && theme === 'light' ? <i className="fas fa-moon icon"></i> : <i className="fas fa-sun icon"></i>}
           </button>
         </span>
 
         <div>
           <a href="https://github.com/ParmentierChristophe">
-            <img className={style.icon} src="/img/GitHub-Mark-32px.png"></img>
+            <i className="fab fa-github icon"></i>
           </a>
           <a href="https://twitter.com/theCrispydesign">
-            <img className={style.icon} src="/img/twitter.png"></img>
+            <i className="fab fa-twitter icon"></i>
           </a>
           <a href="https://www.linkedin.com/in/christophe-parmentier/">
-            <img className={style.icon} src="/img/linkedin.png"></img>
+            <i className="fab fa-linkedin-in icon"></i>
           </a>
         </div>
       </div>
