abf167339af12b889c7dc18037464d214bfc9d7d
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-11-18 10:46:57 +0100

    feat(GA): add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import App from 'next/app';
+import Router from 'next/router';
 import './css/style.css';
+import * as gtag from '../lib/gtag';
+
+Router.events.on('routeChangeComplete', (url) => {
+  window.scrollTo(0, 0);
+  gtag.pageview(url);
+});
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
