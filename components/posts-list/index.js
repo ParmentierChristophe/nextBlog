932dcbad279ca688627c3adbbbf549403fb25375
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-12-09 10:18:26 +0100

    fix codes

--- components/posts-list/index.js
+++ components/posts-list/index.js
@@ -7,31 +7,31 @@ const Posts = ({ slug, posts, paginate }) => {
   const [showMore, setShowMore] = useState(3);
 
   return (
-      <ul className={styles.container}>
-        {posts.slice(0, paginate ? showMore : undefined).map((post) => {
-          return (
-            <TextEntry
-              key={`post-item-${post.slug}`}
-              href="/[slug]"
-              as={`/${post.slug}`}
-              title={`${post.title}`}
-              body={`${post.body}`}
-              category={post.category}
-            />
-          );
-        })}
-        <div className={styles.buttonContainer}>
-          {paginate && showMore < posts.length && (
-            <button
-              className="button -blue center"
-              onClick={() => {
-                setShowMore(showMore + 3);
-              }}>
-              Show More
-            </button>
-          )}
-        </div>
-      </ul>
+    <ul className={styles.container}>
+      {posts.slice(0, paginate ? showMore : undefined).map((post) => {
+        return (
+          <TextEntry
+            key={`post-item-${post.slug}`}
+            href="/[slug]"
+            as={`/${post.slug}`}
+            title={`${post.title}`}
+            body={`${post.body}`}
+            category={post.category}
+          />
+        );
+      })}
+      <div className={styles.buttonContainer}>
+        {paginate && showMore < posts.length && (
+          <button
+            className="button -blue center"
+            onClick={() => {
+              setShowMore(showMore + 3);
+            }}>
+            Show More
+          </button>
+        )}
+      </div>
+    </ul>
   );
 };
 
